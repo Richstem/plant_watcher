8c96e32fe2b41173d76ada30c748276b4e51bbf8
Rich Stem <richstem@gmail.com>
2026-05-07 20:06:15 -0400

    Got button and shell for pump water function. saving for the day

--- lib/plant_watcher_web/live/temperature_live.ex
+++ lib/plant_watcher_web/live/temperature_live.ex
@@ -2,6 +2,7 @@
 defmodule PlantWatcherWeb.TemperatureLive do
   use PlantWatcherWeb, :live_view
 
+  # on mount let's assign a temp value so we know it's awating data
   def mount(_params, _session, socket) do
     # Subscribe to the PubSub topic
     if connected?(socket), do: Phoenix.PubSub.subscribe(PlantWatcher.PubSub, "temp_updates")
@@ -14,7 +15,7 @@ defmodule PlantWatcherWeb.TemperatureLive do
     }
   end
 
-  # This function catches the PubSub message sent from the Channel
+  # Catch the PubSub message sent from the Channel and pass to render fxn
   def handle_info({:new_stats, stats}, socket) do
     {:noreply,
     socket
@@ -24,6 +25,7 @@ defmodule PlantWatcherWeb.TemperatureLive do
     }
   end
 
+  # Hande
   def handle_event("pump_water", {:duration, duration }, socket) do
 
   end
@@ -36,41 +38,49 @@ defmodule PlantWatcherWeb.TemperatureLive do
         justify-content: space-evenly;
         flex-wrap: wrap;
       }
+      .btn-wrapper {
+        display: flex;
+        justify-content: center;
+
+      }
     </style>
 
-    <div class="stats-wrapper">
+    <div>
+      <div class="stats-wrapper">
 
-      <!--Soil temp section -->
-      <div class="text-center mt-10">
-        <h1 class="text-2xl font-bold">Soil Temperature</h1>
-        <div class="text-6xl mt-4 font-mono text-green-600">
-          <%= @soil_temp %>
-        </div>
-      </div>
-
-        <!--Soil moisture section -->
-      <div class="text-center mt-10">
-          <h1 class="text-2xl font-bold">Moisture Reading</h1>
+        <!--Soil temp section -->
+        <div class="text-center mt-10">
+          <h1 class="text-2xl font-bold">Soil Temperature</h1>
           <div class="text-6xl mt-4 font-mono text-green-600">
-            <%= @soil_moisture %>
+            <%= @soil_temp %>
           </div>
-      </div>
+        </div>
 
-      <!--Device temp section -->
-      <div class="text-center mt-10">
-        <h1 class="text-2xl font-bold">Device Temperature</h1>
-        <div class="text-6xl mt-4 font-mono text-green-600">
-          <%= @current_temp %>
+          <!--Soil moisture section -->
+        <div class="text-center mt-10">
+            <h1 class="text-2xl font-bold">Moisture Reading</h1>
+            <div class="text-6xl mt-4 font-mono text-green-600">
+              <%= @soil_moisture %>
+            </div>
+        </div>
+
+        <!--Device temp section -->
+        <div class="text-center mt-10">
+          <h1 class="text-2xl font-bold">Device Temperature</h1>
+          <div class="text-6xl mt-4 font-mono text-green-600">
+            <%= @current_temp %>
+          </div>
         </div>
       </div>
-
-      <button
-      phx-click="pump_water"
-      phx-value-duration="15"
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Water your plants
-    </button>
+      <div class="btn-wrapper my-10">
+        <button
+          phx-click="pump_water"
+          phx-value-duration="15"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Water your plants
+        </button>
       </div>
+
 
     </div>
 
